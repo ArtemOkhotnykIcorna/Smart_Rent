0127ea6668d4d25f842aaeca8146253f831b6ca4
Артем Охотник <artemohotnik2006@gmail.com>
2024-06-13 14:08:17 +0300

    Limit

--- src/controller/filter/index.js
+++ src/controller/filter/index.js
@@ -1,15 +1,19 @@
-const House  = require('../../models/house');
+const House = require('../../models/house');
 
 const getHomeByPrise = async (req, res) => {
     try {
-        const { minPrise, maxPrise}  = req.params
+        const { minPrise, maxPrise } = req.params;
+        const { limit = 200, offset = 0 } = req.query;
 
         const houses = await House.find({
             prise: {
                 $gte: Number(minPrise),
                 $lte: Number(maxPrise)
             }
-        });
+        })
+            .skip(Number(offset))
+            .limit(Number(limit));
+
         res.status(200).json(houses);
     } catch (error) {
         console.error('Error occurred:', error.message);
@@ -18,20 +22,39 @@ const getHomeByPrise = async (req, res) => {
 };
 
 const getHomeByObl = async (req, res) => {
-    try{
-        const { obl } = req.params
+    try {
+        const { obl } = req.params;
+        const { limit = 200, offset = 0 } = req.query;
 
-        const house = await House.findOne({obl: obl})
-        res.status(200).json(house)
-    }
-    catch (error) {
+        const houses = await House.find({ obl: obl })
+            .skip(Number(offset))
+            .limit(Number(limit));
+
+        res.status(200).json(houses);
+    } catch (error) {
         console.error('Error occurred:', error.message);
         res.status(500).json({ error: 'Server error' });
     }
+};
 
-}
+const getHomeByCity = async (req, res) => {
+    try {
+        const { city } = req.params;
+        const { limit = 200, offset = 0 } = req.query;
+
+        const houses = await House.find({ city: city })
+            .skip(Number(offset))
+            .limit(Number(limit));
+
+        res.status(200).json(houses);
+    } catch (error) {
+        console.error('Error occurred:', error.message);
+        res.status(500).json({ error: 'Server error' });
+    }
+};
 
 module.exports = {
     getHomeByPrise,
-    getHomeByObl
+    getHomeByObl,
+    getHomeByCity
 };
--- src/routers/filter.js
+++ src/routers/filter.js
@@ -3,5 +3,6 @@ const filterHouse =  require("../controller/filter/index")
 
 router.get('/api/rent/house/prise/:minPrise/:maxPrise', filterHouse.getHomeByPrise);
 router.get('/api/rent/house/obl/:obl', filterHouse.getHomeByObl);
+router.get('/api/rent/house/city/:city', filterHouse.getHomeByCity);
 
 module.exports = router;
