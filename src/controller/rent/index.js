2233166bc261a3e7d08fdb95c5efc03d97c2c23e
Артем Охотник <artemohotnik2006@gmail.com>
2024-06-21 09:12:12 +0300

    updata bot

--- src/controller/rent/index.js
+++ src/controller/rent/index.js
@@ -4,91 +4,91 @@ const AWS = require('aws-sdk');
 const path = require('path');
 const House  = require('../../models/house');
 
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    endpoint: 'https://s3.filebase.com',
-    signatureVersion: 'v4'
-});
-
-const bucketName = process.env.S3_BUCKET_NAME;
-
-
-// Функція для завантаження фото в S3
-async function uploadPhoto(buffer, fileName) {
-    const params = {
-        Bucket: process.env.S3_BUCKET_NAME,
-        Key: fileName,
-        Body: buffer,
-        ContentType: 'image/jpeg' // Або інший MIME тип відповідно до типу вашого файлу
-    };
-
-    const data = await s3.upload(params).promise();
-    console.log('Successfully uploaded photo ' + fileName + ' to bucket ' + process.env.S3_BUCKET_NAME);
-
-    // Отримання підписаного URL
-    const urlParams = {
-        Bucket: process.env.S3_BUCKET_NAME,
-        Key: fileName,
-        Expires: 60 * 60 // 1 година
-    };
-    const signedUrl = s3.getSignedUrl('getObject', urlParams);
-    console.log('Signed URL: ', signedUrl);
-
-    return signedUrl;
-}
-
-
-const createHouse = async (req, res) => {
-    try {
-        const { address, realtor, realtorPhone, prise, prise_currency, description, photo, district, house_type, balcony, area, floor, walls_type, heating, rooms, children, animals, city, obl} = req.body;
-
-        if (!address || !realtor || !realtorPhone || !prise || !prise_currency || !description || !photo || !district || !house_type || ! balcony || ! area || ! floor || ! walls_type || ! heating || ! rooms || ! children || ! animals || ! city || !obl) {
-            return res.status(400).json({ message: "All fields are required in the request body" });
-        }
-
-        const photoBuffer = Buffer.from(photo, 'base64');
-        const fileName = `${Date.now()}-${address}.jpg`;
-
-        uploadPhoto(photoBuffer, fileName, async (error, signedUrl) => {
-            if (error) {
-                return res.status(500).json({ message: "Error uploading photo" });
-            }
-
-            const house = new House({
-                address: address,
-                realtor: realtor,
-                realtorPhone: realtorPhone,
-                prise: prise,
-                prise_currency: prise_currency,
-                description: description,
-                photo: photo,
-                longitude: longitude,
-                latitude: latitude,
-                district: district,
-                house_type: house_type,
-                balcony: balcony,
-                area: area,
-                floor: floor,
-                walls_type: walls_type,
-                heating: heating,
-                rooms: rooms,
-                children: children,
-                animals: animals,
-                city: city,
-                obl: obl,
-                statusHome: "create"
-            });
-
-            await house.save();
-            console.log("Created new house");
-            res.status(201).json(house);
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Internal server error" });
-    }
-};
+// const s3 = new AWS.S3({
+//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//     endpoint: 'https://s3.filebase.com',
+//     signatureVersion: 'v4'
+// });
+//
+// const bucketName = process.env.S3_BUCKET_NAME;
+//
+//
+// // Функція для завантаження фото в S3
+// async function uploadPhoto(buffer, fileName) {
+//     const params = {
+//         Bucket: process.env.S3_BUCKET_NAME,
+//         Key: fileName,
+//         Body: buffer,
+//         ContentType: 'image/jpeg' // Або інший MIME тип відповідно до типу вашого файлу
+//     };
+//
+//     const data = await s3.upload(params).promise();
+//     console.log('Successfully uploaded photo ' + fileName + ' to bucket ' + process.env.S3_BUCKET_NAME);
+//
+//     // Отримання підписаного URL
+//     const urlParams = {
+//         Bucket: process.env.S3_BUCKET_NAME,
+//         Key: fileName,
+//         Expires: 60 * 60 // 1 година
+//     };
+//     const signedUrl = s3.getSignedUrl('getObject', urlParams);
+//     console.log('Signed URL: ', signedUrl);
+//
+//     return signedUrl;
+// }
+//
+//
+// const createHouse = async (req, res) => {
+//     try {
+//         const { address, realtor, realtorPhone, prise, prise_currency, description, photo, district, house_type, balcony, area, floor, walls_type, heating, rooms, children, animals, city, obl} = req.body;
+//
+//         if (!address || !realtor || !realtorPhone || !prise || !prise_currency || !description || !photo || !district || !house_type || ! balcony || ! area || ! floor || ! walls_type || ! heating || ! rooms || ! children || ! animals || ! city || !obl) {
+//             return res.status(400).json({ message: "All fields are required in the request body" });
+//         }
+//
+//         const photoBuffer = Buffer.from(photo, 'base64');
+//         const fileName = `${Date.now()}-${address}.jpg`;
+//
+//         uploadPhoto(photoBuffer, fileName, async (error, signedUrl) => {
+//             if (error) {
+//                 return res.status(500).json({ message: "Error uploading photo" });
+//             }
+//
+//             const house = new House({
+//                 address: address,
+//                 realtor: realtor,
+//                 realtorPhone: realtorPhone,
+//                 prise: prise,
+//                 prise_currency: prise_currency,
+//                 description: description,
+//                 photo: photo,
+//                 longitude: longitude,
+//                 latitude: latitude,
+//                 district: district,
+//                 house_type: house_type,
+//                 balcony: balcony,
+//                 area: area,
+//                 floor: floor,
+//                 walls_type: walls_type,
+//                 heating: heating,
+//                 rooms: rooms,
+//                 children: children,
+//                 animals: animals,
+//                 city: city,
+//                 obl: obl,
+//                 statusHome: "create"
+//             });
+//
+//             await house.save();
+//             console.log("Created new house");
+//             res.status(201).json(house);
+//         });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ message: "Internal server error" });
+//     }
+// };
 
 const getAllHouse = async (req, res) => {
     try {
@@ -147,9 +147,9 @@ const createHouseBot = async (houseData) => {
 
 
 module.exports = {
-    createHouse,
+    //createHouse,
     getAllHouse,
     findHouseById,
     createHouseBot,
-    uploadPhoto
+    //uploadPhoto
 };
--- src/routers/rent.js
+++ src/routers/rent.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const house =  require("../controller/rent/index")
 
-router.post('/api/rent/house', house.createHouse);
+//router.post('/api/rent/house', house.createHouse);
 router.get('/api/rent/house', house.getAllHouse);
 router.get('/api/rent/house/:id', house.findHouseById);
 
